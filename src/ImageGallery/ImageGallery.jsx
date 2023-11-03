f824054658200d46fd12869e8b2092b2ed9ea8e5
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2023-11-04 03:52:01 +0600

    add multiple image system

---
+++ src/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageGallery.scss'
+
+const ImageGallery = () => {
+    return (
+        <div>
+            sad
+        </div>
+    );
+};
+
+export default ImageGallery;
